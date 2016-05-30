8483b905ea4ed391478d29475cfb072e7d107bb9
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-05-30 12:20:30 +0200

    Restrict access to Kibana for admin
    This commit fixes access to Kibana via
    KibanaProxyView. Otherwise Kibana
    is accessible for non-admin users by typing
    kibana adress directly into browser even if
    button to open it up would be hidden.
    
    Change-Id: I39d3ffc20de6e2e092cc604a25ef6b32ef016b4a

--- monitoring/overview/tests.py
+++ monitoring/overview/tests.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+import urlparse
+
+import mock
 from django.core import urlresolvers
 from django.test import RequestFactory
 from mock import patch, call  # noqa
@@ -29,7 +32,6 @@ class KibanaProxyViewTest(helpers.TestCase):
 
     def test_get_relative_url_with_unicode(self):
         """Tests if it properly converts multibyte characters"""
-        import urlparse
 
         self.view.request = self.request_factory.get(
             '/', data={'a': 1, 'b': 2}
