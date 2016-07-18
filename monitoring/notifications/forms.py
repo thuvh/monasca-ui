60b68d6dc673e5124a8495eb303cdd77cc5f9d31
Brad Klein <bradley.klein@twcable.com>
2016-07-18 15:03:01 -0600

    Update notification create/modify form to include period
    Change-Id: Ib8f16cc700ba15cecf21ef9c64bc5cbd4dabf532

--- monitoring/notifications/forms.py
+++ monitoring/notifications/forms.py
@@ -35,6 +35,7 @@ class BaseNotificationMethodForm(forms.SelfHandlingForm):
     def _init_fields(self, readOnly=False, create=False):
         required = True
         textWidget = None
+        integerWidget = None
         selectWidget = None
         readOnlyTextInput = READONLY_TEXTINPUT
         readOnlySelectInput = forms.Select(attrs={'disabled': 'disabled'})
@@ -61,6 +62,11 @@ class BaseNotificationMethodForm(forms.SelfHandlingForm):
                                                  max_length="512",
                                                  widget=textWidget,
                                                  help_text=_("The email/url address to notify."))
+        self.fields['period'] = forms.IntegerField(label=_("Period"),
+                                                   initial=0,
+                                                   required=required,
+                                                   widget=integerWidget,
+                                                   help_text=_("The notification period."))
 
 
 class CreateMethodForm(BaseNotificationMethodForm):
@@ -88,7 +94,8 @@ class CreateMethodForm(BaseNotificationMethodForm):
                 request,
                 name=data['name'],
                 type=data['type'],
-                address=data['address'])
+                address=data['address'],
+                period=data['period'])
             messages.success(request,
                              _('Notification method has been created '
                                'successfully.'))
@@ -121,6 +128,7 @@ class EditMethodForm(BaseNotificationMethodForm):
             kwargs['name'] = data['name']
             kwargs['type'] = data['type']
             kwargs['address'] = data['address']
+            kwargs['period'] = data['period']
             api.monitor.notification_update(
                 request,
                 **kwargs
