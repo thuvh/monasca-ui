3fbe510038b10becbc698f5082afee2cfbe79c49
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-05-17 14:17:05 +0200

    Integrate with newer client
    In overall following change adjustes monasca-ui code to the
    shape of the python-monascaclient (integrated with osc-lib,
    see Depends-On for details).
    
    Extra:
    - added profiling capabilities
    - added caching of client instances
    - installing master python-monascaclient for test purposes
    - supporting installing projects specified as dependencies using
      Depends-On tag
    
    Depends-On: I1712a24739438e2d8331a495f18f357749a633c5
    Change-Id: I01d0f3d9814376f9358feb23494e1ca5231aedac

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -36,9 +36,13 @@ elif [ -x "$ZUUL_CLONER" ]; then
         /opt/git \
         --branch $BRANCH_NAME \
         git://git.openstack.org \
-        openstack/requirements
-    cd openstack/requirements
-    $install_cmd -e .
+        openstack/requirements \
+        openstack/python-monascaclient
+    cd openstack/requirements ; $install_cmd -e . ; cd -
+    cd openstack/python-monascaclient ; \
+        $install_cmd -e . ; \
+        edit-constraints $localfile -- "python-monascaclient" "-e file://$PWD#egg=python-monascaclient" ; \
+        cd - ;
     popd
 else
     echo "PIP HARDCODE" > /tmp/tox_install.txt
