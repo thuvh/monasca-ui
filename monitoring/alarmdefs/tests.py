6bdef64c70647ba3da8ccfc66f5b1f613863ee77
Artur Basiak <artur.basiak@ts.fujitsu.com>
2017-07-25 09:52:48 +0200

    Fix monasca-ui forms
    Fix problem with unexpected keyword argument 'name' for
    build_attrs() from Django. 'name' was passed to the method
    as a kwargs. **kwargs in build_attrs() were removed.
    Pass 'name' in a dict.
    In Django 1.10+ passing Context to render is deprecated and
    gives an error, pass instead a dict.
    Remove one assertContains from alarm definition create.
    Tags in <input> where generated in random order every time
    the gates run:
    <input type="text" name="name" id="id_name" required maxlength="250" class="form-control" />
    <input type="text" name="name" id="id_name" required class="form-control" maxlength="250" />
    <input type="text" name="name" class="form-control" id="id_name" maxlength="250" required />
    
    Change-Id: I2d39a10f08ac60c163f80cca3256a2bb24b4e0a2
    Story: 2001133
    Task: 4844

--- monitoring/alarmdefs/tests.py
+++ monitoring/alarmdefs/tests.py
@@ -76,9 +76,8 @@ class AlarmDefinitionsTest(helpers.TestCase):
         step = workflow.get_step('setalarmnotificationsaction')
         self.assertIsNotNone(step)
 
-        self.assertContains(res, '<input class="form-control" id="id_name"')
-        self.assertContains(res, '<input class="form-control" '
-                                 'id="id_description"')
+        self.assertContains(res, '<input type="text" name="description" '
+                                 'class="form-control" id="id_description" />')
         self.assertContains(res, '<select class="form-control" '
                                  'id="id_severity"')
 
