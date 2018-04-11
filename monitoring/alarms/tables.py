f84891ec428ec3bd46c617abba4565aefcc7a1bb
Lukasz Zajaczkowski <lukasz.zajaczkowski@ts.fujitsu.com>
2018-04-11 12:55:05 +0200

    Display unique metric names for alarm
    Change-Id: Ia1bfae05e4d4de28689a7cb9495383e328ff51ba
    Story: 2001826
    Task: 12575

--- monitoring/alarms/tables.py
+++ monitoring/alarms/tables.py
@@ -68,9 +68,9 @@ def show_alarm_id(data):
 
 def show_metric_name(data):
     if len(data['metrics']) > 1:
-        names = []
+        names = {}
         for metric in data['metrics']:
-            names.append(metric['name'])
+            names[metric['name']] = ''
         return ', '.join(['%s' % (n) for n in names])
     else:
         return data['metrics'][0]['name']
